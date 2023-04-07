fb0dafc5d7d76da907e3ac93a509c8f548ff4d4e
sneakers-the-rat <JLSaunders987@gmail.com>
2023-04-06 17:11:30 -0700

    Add ratelimit type to logging message

--- config/initializers/rack_attack_logging.rb
+++ config/initializers/rack_attack_logging.rb
@@ -3,5 +3,6 @@ ActiveSupport::Notifications.subscribe(/rack_attack/) do |_name, _start, _finish
 
   next unless [:throttle, :blacklist].include? req.env['rack.attack.match_type']
 
-  Rails.logger.info("Rate limit hit (#{req.env['rack.attack.match_type']}): #{req.ip} #{req.request_method} #{req.fullpath}")
+  match_data = req.env['rack.attack.match_data']
+  Rails.logger.info("Rate limit hit (#{req.env['rack.attack.match_type']} - #{match_data[:limit].to_s}): #{req.ip} #{req.request_method} #{req.fullpath}")
 end
